39b8dc8f4973842bc040ba3e4af72c17ec72cdc8
anthonyshadow <anthony.shadowitz@gmail.com>
2022-02-16 11:49:41 -0500

    setup html template to render python file

--- main.py
+++ main.py
@@ -3,15 +3,18 @@ import web
 
 #Routes for Url to run 
 urls = (
-    '/(.*)', 'index'
+    '/(.*)/(.*)', 'index'
 )
+
+#where to find templates
+render = web.template.render("resources/")
 
 #substantiate the app
 app = web.application(urls, globals())
 
 class index: 
-    def GET(self, name):
-        return "<h1>Hello, "+ name + "how are you today?</h1>"
+    def GET(self, name, age):
+        return render.main(name, age)
 
 if __name__ == "__main__":
     app.run()
